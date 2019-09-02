5f27dd5e8bd45cbf6097976cefc4a33f2e4e6f92
Jan Skowronski <crianonim@gmail.com>
2019-09-02 08:32:25 +0100

    Add listonosz project to data

--- src/data/projects.js
+++ src/data/projects.js
@@ -18,5 +18,25 @@ export const projects = [
           "(progressive web app)",
       ]
     
-    }
+    },
+    { 
+        name: "Listonosz" ,
+        description:"Postman clone run as a web app either locally or hosted in the cloud.",
+        github:"https://github.com/crianonim/listonosz",
+        live:"https://listonosz-postman.herokuapp.com/",
+        screenshot:"https://camo.githubusercontent.com/5c4b95cc45329b013f26778e14c84a07bd2dff38/68747470733a2f2f692e696d6775722e636f6d2f7072334e364d582e706e67",
+        problem:`I want to be able to test API endpoints. I don't want to run a heavy, feature rich app like postman as I need basic functionality.
+        I need to be able to make request easily, selecting a HTTP method, pass parameters and set headers. I would like Authorisation to be easy to set up.`,
+        tech:[
+            "Express.js to deal with routing to endpoints",
+            "Vue.js frontend",
+            "Vuetify component library"
+        ],
+        learning:[
+            "Dealing with API calls",
+            "Vue.js framework",
+            "Using a component library to create a consistent and professional look",
+        ]
+      
+      }
 ];
