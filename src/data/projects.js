3b2ace664437406a570c07570a3fee192144cd71
Jan Skowronski <crianonim@gmail.com>
2019-09-02 08:16:47 +0100

    Add first project to data

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,3 +1,22 @@
-export const projects=[
+export const projects = [
+    { 
+      name: "BusLonBus" ,
+      description:"Simple and efficient London Bus app written in pure vanilla JavaScript using lastest features.",
+      github:"https://github.com/crianonim/buslonbus",
+      live:"https://crianonim.github.io/buslonbus/",
+      screenshot:"https://camo.githubusercontent.com/f2e6ffdb23601b6f87f402c887167ebc9e6aaf74/68747470733a2f2f692e696d6775722e636f6d2f5376786f5153452e706e67",
+      problem:`I want to be quickly find next arrivals at selected bus stop, chosen based on my location or code displayed at the stop.
+       I would like to bookmark selected stops and fetch fresh predictions when I want.
+      I need this app to be fast and lightweight, working with and without GPS and even poor internet access.`,
+      tech:[
+          "Plain vanilla js, not meant to be comapatible with all browsers but to utilise the latest in web technologies"
+      ],
+      learning:[
+          "HTML template element",
+          "Custom elements",
+          "It is not always easy to style custom elements protected with shadow root",
+          "(progressive web app)",
+      ]
     
-]
+    }
+];
