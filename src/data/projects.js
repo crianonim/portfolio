b8f3b8546126a164b015ee2c8406ab99a5c76dba
Jan Skowronski <crianonim@gmail.com>
2019-09-03 08:41:47 +0100

    Refactor project problem data into array

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,9 +5,12 @@ export const projects = [
       github:"https://github.com/crianonim/buslonbus",
       live:"https://crianonim.github.io/buslonbus/",
       screenshot:"https://camo.githubusercontent.com/f2e6ffdb23601b6f87f402c887167ebc9e6aaf74/68747470733a2f2f692e696d6775722e636f6d2f5376786f5153452e706e67",
-      problem:`I want to be quickly find next arrivals at selected bus stop, chosen based on my location or code displayed at the stop.
-       I would like to bookmark selected stops and fetch fresh predictions when I want.
-      I need this app to be fast and lightweight, working with and without GPS and even poor internet access.`,
+      problem:[
+        `quickly find next arrivals at selected bus stop, chosen based on my location or code displayed at the stop`,
+        `bookmark selected stops`,
+        `update/fetch fresh predictions when I want`,
+        `have an app that is fast and lightweight, working with and without GPS and even poor internet access`
+      ],
       tech:[
           "Plain vanilla js, not meant to be comapatible with all browsers but to utilise the latest in web technologies"
       ],
@@ -27,8 +30,12 @@ export const projects = [
         github:"https://github.com/crianonim/listonosz",
         live:"https://listonosz-postman.herokuapp.com/",
         screenshot:"https://camo.githubusercontent.com/5c4b95cc45329b013f26778e14c84a07bd2dff38/68747470733a2f2f692e696d6775722e636f6d2f7072334e364d582e706e67",
-        problem:`I want to be able to test API endpoints. I don't want to run a heavy, feature rich app like postman as I need basic functionality.
-        I need to be able to make request easily, selecting a HTTP method, pass parameters and set headers. I would like Authorisation to be easy to set up.`,
+        problem:[
+         `be able to test API endpoints`,
+         `be able to make request easily, selecting a HTTP method, url, pass parameters and set headers`,
+         `have an easy way to set up Authorisation if needed`,
+         `have an lightweight web app that covers my basic needs as opposed to heavy and feature rich app like postman`   
+        ],
         tech:[
             "Express.js to deal with routing to endpoints",
             "Vue.js frontend",
