e222b4bd8a6eee6c8bc94c65b0e4685d4ba5a28e
Jan Skowronski <crianonim@gmail.com>
2019-09-06 09:40:07 +0100

    Add title tooltip to stack icons

--- src/components/StackIcons.js
+++ src/components/StackIcons.js
@@ -11,17 +11,17 @@ import SVGIcon from '../assets/svg.svg'
 import './StackIcons.sass';
 
 const stackIcons=[
-  {name:"vue",type:"fa",data:faVuejs},
-  {name:"express",type:"svg",data:ExpressIcon},
-  {name:"html",type:"fa",data:faHtml5},
-  {name:"node",type:"fa",data:faNodeJs},
-  {name:"js",type:"fa",data:faJs},
-  {name:"vuetify",type:"svg",data:VuetifyIcon},
-  {name:"react",type:"fa",data:faReact},
-  {name:"redux",type:"svg",data:ReduxIcon},
-  {name:"material",type:"svg",data:MaterialIcon},
-  {name:"svg",type:"svg",data:SVGIcon},
-  {name:"sass",type:"fa",data:faSass}
+  {name:"vue",type:"fa",data:faVuejs, title:"Vue.js"},
+  {name:"express",type:"svg",data:ExpressIcon, title:"Express.js"},
+  {name:"html",type:"fa",data:faHtml5, title:"HTML5"},
+  {name:"node",type:"fa",data:faNodeJs, title:"Node.js"},
+  {name:"js",type:"fa",data:faJs,title:"Vanilla JavaScript"},
+  {name:"vuetify",type:"svg",data:VuetifyIcon,title:"Vuetify Material Component Library"},
+  {name:"react",type:"fa",data:faReact,title:"React"},
+  {name:"redux",type:"svg",data:ReduxIcon,title:"Redux"},
+  {name:"material",type:"svg",data:MaterialIcon,title:"Material UI Material Component Library for React"},
+  {name:"svg",type:"svg",data:SVGIcon,title:"Scalable Vector Graphic"},
+  {name:"sass",type:"fa",data:faSass,title:"Sass"}
 ]
 
 export default ({stack})=>{
@@ -30,8 +30,8 @@ export default ({stack})=>{
                 {stack.map(name=>stackIcons.find(el=>el.name===name))
                   .filter(Boolean)
                   .map( (stack,i)=>stack.type==='fa'?
-                   <FontAwesomeIcon key={i} size="2x" icon={stack.data}/>:
-                   <img src={stack.data} key={i}  alt="stack.name"/>
+                   <FontAwesomeIcon title={stack.title} key={i} size="2x" icon={stack.data}/>:
+                   <img src={stack.data} key={i}  title={stack.title} alt="stack.name"/>
                    )}
         </div>
 
