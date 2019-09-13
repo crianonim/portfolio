6eb2fea6ef2286af5091b6331431f455044bbcd4
Jan Skowronski <crianonim@gmail.com>
2019-09-13 07:39:56 +0100

    Add photo and style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,12 @@
 import React from "react";
 import "./Header.sass";
 export default () => {
-  return (<div>
-      <h1>Jan Skowronski</h1>
-  </div>
+  return (<header>
+      <img className="my-photo" alt="Jan Skowronski" src="img/jan.jpeg" />
+      <div className="header-text">
+          <h1>Jan Skowronski</h1> 
+          <h2>software developer</h2>
+      </div>
+  </header>
   )
 }
