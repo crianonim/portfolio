6a7d59cfcbd6d42bd9e6f3be89e90de8a4f3264a
Jan Skowronski <crianonim@gmail.com>
2019-09-14 11:40:17 +0100

    Change toggle button

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Header.sass";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
-import {faGlobe, faEnvelope, faPhone} from '@fortawesome/free-solid-svg-icons';
+import {faGlobe, faEnvelope, faPhone, faAdjust} from '@fortawesome/free-solid-svg-icons';
 
 const toggleTheme = ()=>{
     document.querySelector('body').classList.toggle('dark')
@@ -14,7 +14,9 @@ export default () => {
           <div className="top">
             <h1>Jan Skowronski</h1> 
             <h2>software developer</h2>
-            <button onClick={toggleTheme}>Theme</button>
+            <h3>
+            <FontAwesomeIcon title="Change theme" className="adjust-button" icon={faAdjust} onClick={toggleTheme}/>
+            </h3>
           </div>
           <h2 className="contact">
           <a href="https://github.com/crianonim" rel="noopener noreferrer" target="_blank">
