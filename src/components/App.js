16a595c9aa9b7576ab7e4fd16264ed0e01690617
Jan Skowronski <crianonim@gmail.com>
2019-09-14 18:19:11 +0100

    Add TabbedContent

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import Bio from './Bio';
-import ProjectCard from './ProjectCard';
-import {projects} from '../data/projects'; 
+
 import Header from './Header';
+import TabbedContent from './TabbedContent';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Bio/>
-      {projects.map( (project,i)=><ProjectCard key={i} project={project}/>)}
+      <TabbedContent />
+      
     </div>
   );
 }
