392de77ad0e94015dc425f73a31dcff89e4e6f51
Jan Skowronski <crianonim@gmail.com>
2019-09-15 10:58:12 +0100

    Add interests data

---
+++ src/data/interests.js
@@ -0,0 +1,47 @@
+export const interests = [
+    { 
+      name: "Languages" ,
+      img: "languages",
+      content:[
+          "Since childhood, I've loved learning languages. In London, I just kept going thanks to a delicious mix of cultures and languages that piqued my curiosity even more.",
+          "I'm fluent in Portugues do Brasil, can have una buena conversación en español, order food in un ristorante italiano",
+          "I like to think that my JavaScript is quite fluent as well..."
+      ]
+    },
+    {
+        name:"Botany",
+        img:"botany",
+        content:[
+            "Nothing brings me more joy then watching something grow! Especially if it's something I planted, sowed, started from scratch.",
+            "Then with careful cultivation, it develops through different stages into a new, living thing.",
+            "I like to look into a mechanism (plant cell) and try to understand how does it actually work."
+        ]
+    },
+    { 
+        name: "Cooking" ,
+        img: "cooking",
+        content:[
+            "I like learning new recipes (algorithms?) form others and improve my craft. I like as well to experiment and come up with new.",
+            "The best is that you share the fruit of your labour and form a little community even if ephemeral and limited in space to your dining table.",
+            "Oh, and I like to eat!"
+        ]
+      },
+      {
+          name:"History",
+          img:"history",
+          content:[
+              "Magna Carta, the Tudor court, colonialism? I love to learn about it all. Studying history lets us learn from the experience of others in the past.",
+              "People had so many great ideas we can copy, and have done horrible mistakes we should avoid.",
+              "It's an exciting narrative as well. Life is stranger than fiction."
+          ]
+      },
+      {
+        name:"Technology",
+        img:"technology",
+        content:[
+            "Technology lets me transcend my biology. I can communicate with others without space limitations.",
+            "I am able to interact with my internal creativity, be equal to ancient gods with my powers to make things as I see fit with code.",
+            "I relax playing video games (there are games that let me farm, cook, tell a story using a foreign language, ticking all my hobbies). Technology is fun!"
+        ]
+    }
+]
